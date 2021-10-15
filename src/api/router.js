0d517b91dc8bb7207c65646249f418fc9a204467
Nikita Chursin <chursinn91@gmail.com>
2021-10-15 14:16:31 +0300

    introduced fallback error

--- src/api/router.js
+++ src/api/router.js
@@ -10,10 +10,12 @@ const router = (app, config) => {
   app.post("/generate/", handle(config, generate.post));
 };
 
-const handle = (config, handler) => async (req, res) => {
-  //console.log("handler >>", req);
-  //handler(config, req).then((data) => res.send(data));
-  res.send(await handler(config, req)); //.catch(next);
+const handle = (config, handler) => async (req, res, next) => {
+  try {
+    res.send(await handler(config, req));
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = router;
